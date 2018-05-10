e21e9cbcdc97b28d9d493dcaf973ec41daafd9ec
fongha <35597502+fongha@users.noreply.github.com>
2018-05-10 21:17:51 +0200

    removed unneccessary structs

--- src/main.go
+++ src/main.go
@@ -12,53 +12,27 @@ import (
 
 type L struct {
 	Cod     string  `json:"cod"`
-	Message float64 `json:"message"`
-	Cnt     int     `json:"cnt"`
 	List    []struct {
 		Dt   int `json:"dt"`
 		Main struct {
 			Temp      float64 `json:"temp"`
-			TempMin   float64 `json:"temp_min"`
-			TempMax   float64 `json:"temp_max"`
-			Pressure  float64 `json:"pressure"`
-			SeaLevel  float64 `json:"sea_level"`
-			GrndLevel float64 `json:"grnd_level"`
-			Humidity  int     `json:"humidity"`
-			TempKf    float64 `json:"temp_kf"`
 			Celsius	   float64
 			Comment		string
 			Time 		string
 		} `json:"main"`
 		Weather []struct {
-			ID          int    `json:"id"`
 			Main        string `json:"main"`
 			Description string `json:"description"`
 			Icon        string `json:"icon"`
 		} `json:"weather"`
-		Clouds struct {
-			All int `json:"all"`
-		} `json:"clouds"`
 		Wind struct {
 			Speed float64 `json:"speed"`
-			Deg   float64 `json:"deg"`
 		} `json:"wind"`
-		Rain struct {
-			ThreeH float64 `json:"3h"`
-		} `json:"rain"`
-		Sys struct {
-			Pod string `json:"pod"`
-		} `json:"sys"`
 		DtTxt string `json:"dt_txt"`
 	} `json:"list"`
 	City struct {
-		ID    int    `json:"id"`
 		Name  string `json:"name"`
-		Coord struct {
-			Lat float64 `json:"lat"`
-			Lon float64 `json:"lon"`
-		} `json:"coord"`
 		Country    string `json:"country"`
-		Population int    `json:"population"`
 	} `json:"city"`
 }
 
